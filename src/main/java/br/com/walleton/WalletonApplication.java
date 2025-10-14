f31555ab6045a8df3dbd55e9aeca9d79144f70c7
Adams Wildemann <adams.contato@gmail.com>
2025-10-14 20:14:29 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/walleton/WalletonApplication.java
@@ -0,0 +1,13 @@
+package br.com.walleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletonApplication.class, args);
+    }
+
+}
